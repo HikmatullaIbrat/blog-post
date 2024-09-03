7c1ef12232afea3376d5250b61edc773e3e16a22
hikmatullaibrat <hikmatullah.ibrat2018@gmail.com>
2024-09-03 11:02:13 +0430

    soft Delete functionality added

---
+++ database/migrations/2024_09_03_044543_add_soft_delete_to_posts.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // softDelete is added with below line
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // soft delete will work with deleted_at column in database 
+            $table->dropColumn('deleted_at');
+        });
+    }
+};
